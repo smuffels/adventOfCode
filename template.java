bc040fbf4a43158450b2ac8e089635399ac0ec6b
celine <bruncel1@students.zhaw.ch>
2022-12-04 13:21:58 +0100

    add template

---
+++ template.java
@@ -0,0 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class template {
+    public static void main(String[] args) throws Exception {
+        String inputT = Files.readString(Path.of("C:\\Users\\brunc\\OneDrive - ZHAW\\1.Semester\\AdventOfCode\\adventOfCode\\input\\day5Test.txt"));
+        String input = Files.readString(Path.of("C:\\Users\\brunc\\OneDrive - ZHAW\\1.Semester\\AdventOfCode\\adventOfCode\\input\\day5.txt"));
+
+
+        int sum = 0;
+        //split the string into an array of strings
+        String[] lines = input.split("\r\n");
+
+        System.out.println(lines);
+    }
+}
